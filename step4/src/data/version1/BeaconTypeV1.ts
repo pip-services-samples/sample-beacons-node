28b1c3242801e5df12a68a2be29007b2164b7185
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2018-09-05 20:24:12 -0600

    Minor erors

--- step4/src/data/version1/BeaconTypeV1.ts
+++ step4/src/data/version1/BeaconTypeV1.ts
@@ -1,5 +1,5 @@
 export class BeaconTypeV1 {
-    public static Unknown: string = "unkown";
+    public static Unknown: string = "unknown";
     public static AltBeacon: string = "altbeacon";
     public static iBeacon: string = "ibeacon";
     public static EddyStoneUdi: string = "eddystone-udi";
